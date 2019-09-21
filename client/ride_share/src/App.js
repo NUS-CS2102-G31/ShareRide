7f83313426e869875c5c9a9b7c9206d19fdfee05
raymondvictorioh <raymondvictorioh@gmail.com>
2019-09-21 22:01:20 +0800

    added Profile and Profiles component

--- client/ride_share/src/App.js
+++ client/ride_share/src/App.js
@@ -3,36 +3,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Register from './register/Register';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Profile from './profile/Profile';
+import Profiles from './profile/Profiles'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
-        {/* <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="/hello"
-            target="_blank"
-            rel="noopener noreferrer"
-          > }
-            Learn React
-          </a>
-
-        </header>
-      </div> */}
-      <Link to="/signup">Register</Link>
-
-     
+      <Link to="/signup" > Register </Link>
+      <Link to="/profile" >Profile</Link>
       <Route path="/signup" component={Register} />
-      
-      
-    </Router>
-  );
+      <Route path="/profile" component={Profiles} />
+    </Router>);
 }
 
 export default App;
