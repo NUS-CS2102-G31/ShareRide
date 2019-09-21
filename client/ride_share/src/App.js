b6c87acd8929b5306261572fc29ad6c4f90b983b
Matthew Alexander <andyrobert23@Matthews-MacBook-Pro.local>
2019-09-21 19:46:06 +0800

    Router class for pages

--- client/ride_share/src/App.js
+++ client/ride_share/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
+// import Register from './register/Register';
 import logo from './logo.svg';
 import './App.css';
+import Register from './register/Register';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="/hello"
+            target="_blank"
+            rel="noopener noreferrer"
+          > }
+            Learn React
+          </a>
+
+        </header>
+      </div> */}
+      <Link to="/signup">Register</Link>
+
+     
+      <Route path="/signup" component={Register} />
+      
+      
+    </Router>
   );
 }
 
