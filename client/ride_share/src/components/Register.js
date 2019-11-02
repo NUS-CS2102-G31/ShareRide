78cc77462d7a1e21256b696a1441473a5be67dc1
Raymond Victorio Handoko <e0323109@u.nus.edu>
2019-11-02 11:46:11 +0800

    shareride client with responsive navbar

--- client/ride_share/src/components/Register.js
+++ client/ride_share/src/components/Register.js
@@ -63,43 +63,44 @@ export default class Register extends React.Component {
   handleSubmit = event => {
     // Post request to backend
     event.preventDefault();
-    console.log(this.state)
+    console.log(event)
     fetch('http://localhost:3600/api/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        email: this.state.email,
+        username: this.state.email,
         password: this.state.password,
       })
     })
-    .then(res => console.log(res));
+    // .then(res => alert(res.body.message));
+    alert(`User has been created`)
   }
 
   renderForm() {
     return (
       <div className="form">
         <Form onSubmit={this.handleSubmit}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
           </Form.Text>
-        </Form.Group>
-      
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
         </Button>
-      </Form>
+        </Form>
       </div>
     );
   }
